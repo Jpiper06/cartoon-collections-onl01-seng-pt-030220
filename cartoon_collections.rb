2bee4eb54410d681fe922c9345e9f15de3f2f469
Jaquan Piper <jaquan.piper@gmail.com>
2020-03-14 21:06:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -4,10 +4,10 @@ def roll_call_dwarves(dwarves)
   end
 end 
 
-def summon_captain_planet(planeter_calls)
-  #planeteer_calls.collect do |call|
-    #call.capitalize <<"!"
-  #end
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.collect do |call|
+    "#{call.capitalize}!"
+  end
 end 
 
 def long_planeteer_calls(calls)
